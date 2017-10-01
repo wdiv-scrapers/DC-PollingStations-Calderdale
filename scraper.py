9a73b6948f9a33278392f4b16551862534c313bf
chris48s <chris.shaw480@gmail.com>
2017-10-01 11:27:41 +0100

    sync resource json to GitHub
    Use CkanScraper to sync to GitHub
    and HashOnlyScraper to store hash on morph

--- scraper.py
+++ scraper.py
@@ -1,10 +1,43 @@
+from dc_base_scrapers.ckan_scraper import CkanScraper
 from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 
 
-stations_url = "http://dataworks.calderdale.gov.uk/api/3/action/package_show?id=polling-stations"
-districts_url = "http://dataworks.calderdale.gov.uk/api/3/action/package_show?id=polling-station-districts"
+base_url = 'http://dataworks.calderdale.gov.uk/api/3/action/package_show?id='
+
+stations_info = {
+    'dataset': 'polling-stations',
+    'extra_fields': [],
+    'return_format': 'json',
+}
+
+districts_info = {
+    'dataset': 'polling-station-districts',
+    'extra_fields': [],
+    'return_format': 'json'
+}
+
 council_id = 'E08000033'
 
+stations_meta_scraper = CkanScraper(
+    base_url,
+    council_id,
+    stations_info['dataset'],
+    stations_info['return_format'],
+    stations_info['extra_fields'],
+    'utf-8')
+stations_meta_scraper.scrape()
+
+districts_meta_scraper = CkanScraper(
+    base_url,
+    council_id,
+    districts_info['dataset'],
+    districts_info['return_format'],
+    districts_info['extra_fields'],
+    'utf-8')
+districts_meta_scraper.scrape()
+
+stations_url = "%s%s" % (base_url, stations_info['dataset'])
+districts_url = "%s%s" % (base_url, districts_info['dataset'])
 
 stations_scraper = HashOnlyScraper(stations_url, council_id, 'stations')
 stations_scraper.scrape()
