a0e3a926baf6fc9e6a33b629e9e569f14bf89dfd
chris48s <chris.shaw480@gmail.com>
2018-01-13 17:28:44 +0000

    update to use DataPressScraper

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
-from dc_base_scrapers.ckan_scraper import CkanScraper
+from dc_base_scrapers.datapress_scraper import DataPressScraper
 from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 
 
-base_url = 'http://dataworks.calderdale.gov.uk/api/3/action/package_show?id='
+base_url = 'https://dataworks.calderdale.gov.uk/api/dataset/'
 
 stations_info = {
     'dataset': 'polling-stations',
@@ -18,7 +18,7 @@ districts_info = {
 
 council_id = 'E08000033'
 
-stations_meta_scraper = CkanScraper(
+stations_meta_scraper = DataPressScraper(
     base_url,
     council_id,
     stations_info['dataset'],
@@ -27,7 +27,7 @@ stations_meta_scraper = CkanScraper(
     'utf-8')
 stations_meta_scraper.scrape()
 
-districts_meta_scraper = CkanScraper(
+districts_meta_scraper = DataPressScraper(
     base_url,
     council_id,
     districts_info['dataset'],
